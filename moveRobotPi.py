a3ba33607fff8b06cefbe33109da0cb5f4c7ee9a
Nakul Srikanth <nakul_0702@yahoo.com>
2023-10-18 14:08:45 -0700

    Draft 5

--- moveRobotPi.py
+++ moveRobotPi.py
@@ -1,30 +1,34 @@
 from pymycobot.mycobot import MyCobot
 import time
 
-board_size = 250
+board_size = 150
 dim = 3
 
 
 def play(before, after):
     mc = MyCobot("/dev/ttyAMA0", 1000000)
     
-    square_length = 250/3
+    square_length = board_size/dim
+    
+    delay = 3
 
-    x = ((before[0] - 1) * square_length) - 125
-    y = (before[1] - 1) * square_length
+    x = ((before[0] - 1) * square_length) - 75
+    y = ((before[1] - 1) * square_length) + 120
     z = 150
 
-    rx = 170
-    ry = 5
-    rz = -50
+    rx = -170
+    ry = -5
+    rz = 125
+
+    print(x, y, z)
 
     mc.send_coords([x, y, z, rx, ry, rz], 20)
-    time.sleep(3)
+    time.sleep(delay)
     mc.send_coords([x, y, z, 100, rx, ry, rz], 20)
-    time.sleep(3)
+    time.sleep(delay)
 
     mc.set_gripper_state(0, 20)
-    time.sleep(3)
+    time.sleep(delay)
 
     if after == None:
         after = [4.5, 1.5]
@@ -34,11 +38,11 @@ def play(before, after):
     z = 150
 
     mc.send_coords([x, y, z, rx, ry, rz], 20)
-    time.sleep(3)
+    time.sleep(delay)
     mc.send_coords([x, y, z, 100, rx, ry, rz], 20)
-    time.sleep(3)
+    time.sleep(delay)
     mc.set_gripper_state(1, 20)
-    time.sleep(3)
+    time.sleep(delay)
 
     mc.send_coords([0, 100, z, rx, ry, rz], 20)
-    time.sleep(3)
+    time.sleep(delay)
