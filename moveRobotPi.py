2c73b4076c5e0407b61b20b79547af935f79019a
Nakul Srikanth <nakul_0702@yahoo.com>
2023-10-19 15:48:53 -0700

    Draft 10

--- moveRobotPi.py
+++ moveRobotPi.py
@@ -24,10 +24,11 @@ def play(before, after):
 
     mc.send_coords([x, y, z, rx, ry, rz], 20)
     time.sleep(delay)
-    mc.send_coords([x, y, 100, rx, ry, rz], 20)
+    mc.send_coords([x, y, 125, rx, ry, rz], 20)
     time.sleep(delay)
 
-    mc.set_gripper_state(1, 20)
+    mc.set_gripper_value(97, 20)
+    #mc.set_gripper_state(1, 20)
     time.sleep(delay)
 
     if after == None:
@@ -39,7 +40,7 @@ def play(before, after):
 
     print("After: ", x, y, z)
 
-    mc.send_coords([x, y, 100, rx, ry, rz], 20)
+    mc.send_coords([x, y, 125, rx, ry, rz], 20)
     time.sleep(delay)
     mc.set_gripper_state(0, 20)
     time.sleep(delay)
