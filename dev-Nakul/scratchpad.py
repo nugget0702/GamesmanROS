d0cfc0b6f3cfe0349510114d15b3bdf0d46975cf
Nakul Srikanth <nakul_0702@yahoo.com>
2024-04-06 14:33:15 -0700

    Test9.33

--- dev-Nakul/scratchpad.py
+++ dev-Nakul/scratchpad.py
@@ -1,14 +1,14 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-# Quaternion for +180 degrees rotation around Z-axis
-q_z = R.from_quat([0, 0, 0, 1])
+# Quaternion for -90 degrees rotation around Z-axis (clockwise looking down)
+q_z = R.from_quat([0, 0, np.sqrt(2)/2, -np.sqrt(2)/2])
 
-# Quaternion for 180 degrees rotation around X-axis
-q_x = R.from_quat([0, 1, 0, 0])
+# Quaternion for 180 degrees rotation around Y-axis
+q_y = R.from_quat([0, 1, 0, 0])
 
-# Combine the rotations
-combined_q = q_x * q_z
+# Combine the rotations, respecting the sequence
+combined_q = q_y * q_z
 
 # Output the combined quaternion
 combined_quaternion = combined_q.as_quat()
