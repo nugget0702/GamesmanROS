54c1d4cf3691d4582d2058b00c91886c32f7a996
Nakul Srikanth <nakul_0702@yahoo.com>
2024-04-11 22:43:02 -0700

    Test9.40

--- dev-Nakul/observe.py
+++ dev-Nakul/observe.py
@@ -1,6 +1,6 @@
 import pymycobot
 
-observe = [126.65, 9.31, -80.85, -13.97, 11.95, 88.76]
+observe = [131.13, 5.53, -50.53, -37.08, 13.27, 91.93]
 
 mc = pymycobot.MyCobot("/dev/ttyAMA0", baudrate=1000000)
 mc.send_angles(observe, 20)
