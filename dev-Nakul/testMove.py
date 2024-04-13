b216975db108d70a6059212f135c196f98920610
Nakul Srikanth <nakul_0702@yahoo.com>
2024-04-13 08:13:19 -0700

    Test10.2

--- dev-Nakul/testMove.py
+++ dev-Nakul/testMove.py
@@ -7,9 +7,9 @@ piece_pose = Pose()
 piece_pose.position.x = -0.002
 piece_pose.position.y = 0.20
 piece_pose.position.z = 0.20
-piece_pose.orientation.x = 0.5
-piece_pose.orientation.y = -0.5
-piece_pose.orientation.z = -0.5 
-piece_pose.orientation.w = 0.5
+piece_pose.orientation.x = 0
+piece_pose.orientation.y = 1
+piece_pose.orientation.z = 0 
+piece_pose.orientation.w = 0
 
 acutation.move(piece_pose)
